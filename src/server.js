b87626d335f7597a53c63756eafbdbc28aed0c92
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-06 00:55:07 -0300

    regex para validação e identificar parametros da rota

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
+// CommonJS => required
+// ESModules => import/export
+
 import http from 'node:http'
 
 import { json } from './middlewares/json.js'
 import {routes} from './routes.js'
 
-// CommonJS => required
-// ESModules => import/export
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
     const route = routes.find(route => {
-        return route.method === method && route.path === url
+        return route.method === method && route.path.test(url)
     })
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+        
         return route.handler(req, res)
     }
 
