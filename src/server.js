116b21fde1092ed86f0955c84ec712074ec4a880
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-05 23:45:39 -0300

    simulação de banco de dados em json e separação das rotas

--- src/server.js
+++ src/server.js
@@ -1,35 +1,21 @@
 import http from 'node:http'
+
 import { json } from './middlewares/json.js'
+import {routes} from './routes.js'
+
 // CommonJS => required
 // ESModules => import/export
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
-
-const users = []
-
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
-        return res
-        .setHeader('Content-Type', 'application/json')
-        .end(JSON.stringify(users))
-    }
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-
-        users.push({
-            id: users.length + 1,
-            name,
-            email
-        })
-
-        return res.writeHead(201).end();
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
